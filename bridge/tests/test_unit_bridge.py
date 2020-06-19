3bcceb0553897902772f7ff79e66f2259f451d15
prin-r <q@bandprotocol.com>
2020-06-19 20:33:27 +0700

    -

---
+++ bridge/tests/test_unit_bridge.py
@@ -0,0 +1,12 @@
+from ..bridge import BRIDGE
+from tbears.libs.scoretest.score_test_case import ScoreTestCase
+
+
+class TestBRIDGE(ScoreTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.score = self.get_score_instance(BRIDGE, self.test_account1)
+
+    def test_hello(self):
+        self.assertEqual(self.score.hello(), "Hello")
